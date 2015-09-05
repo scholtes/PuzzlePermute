368a35a5c2e34a992dd57ebd763a0ee0e1e9a6a2
scholtes <scholtes.garrett@gmail.com>
2015-09-05 00:47:55 -0400

    Setting up command line params

--- PuzzleMain.java
+++ PuzzleMain.java
@@ -4,7 +4,19 @@ public class PuzzleMain {
 
     public static void main(String[] args) {
         
-        System.out.println("Testing...");
+        if(args.length == 0) {
+            System.out.println("Usage:");
+            System.out.println("java PuzzleMain IMGS [-v]");
+            System.out.println("    IMGS:    directory to look for .png puzzle pieces");
+            System.out.println("    -v:      verbose if flag is present");
+        } else {
+            //TODO: check for valid directory here
+            if(args.length == 2) {
+                //TOOD: check for valid flag
+            } else if(args.length > 2) {
+                //TODO: warn unrecongnized flags
+            }
+        }
 
     }
 
